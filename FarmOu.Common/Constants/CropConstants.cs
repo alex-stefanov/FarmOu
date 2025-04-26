8f7bceeb1fd047e7a6e4dcc6fb6254a71a4b7c60
alex-stefanov <rlgalexbgto@gmail.com>
2025-04-26 17:20:37 +0300

    Added common layer and introduced some constants

---
+++ FarmOu.Common/Constants/CropConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace FarmOu.Common.Constants;
+
+/// <summary>
+/// Contains constants related to crop name, such as maximum and minimum lengths.
+/// </summary>
+public class CropConstants
+{
+    /// <summary>
+    /// The maximum allowed length for a crop's name.
+    /// </summary>
+    public const int NameMaxLength = 80;
+
+    /// <summary>
+    /// The minimum required length for a crop's name.
+    /// </summary>
+    public const int NameMinLength = 3;
+}
---
+++ FarmOu.Common/Constants/ToolConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace FarmOu.Common.Constants;
+
+/// <summary>
+/// Contains constants related to tool name, such as maximum and minimum lengths.
+/// </summary>
+public class ToolConstants
+{
+    /// <summary>
+    /// The maximum allowed length for a tool's name.
+    /// </summary>
+    public const int NameMaxLength = 80;
+
+    /// <summary>
+    /// The minimum required length for a tool's name.
+    /// </summary>
+    public const int NameMinLength = 3;
+}
